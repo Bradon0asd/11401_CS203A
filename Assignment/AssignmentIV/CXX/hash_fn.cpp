20488611d1bd678b115e8ca33d1d5cee457fa46e
chiachen0asd <chiang930630@gmail.com>
2025-11-30 20:22:30 +0800

    docs(hash_fn.cpp): And commit and reference

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -7,7 +7,7 @@
     This file contains the hash functions for integer and string keys.
 
    Development History:
-    - 2025/11/11: Initial implementation
+    - 2025/11/26: Initial implementation
     - 2025/11/30: Refactored to use hash_fn.hpp
 
    Developer: Kuna-Ti Chiang <s1123306@mail.yzu.edu.tw>
@@ -16,9 +16,9 @@
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
-    const double A = 0.6180339887498948482;
-    double v = key * A;
-    double fractional = v - (long long)v;
+    const double A = 0.6180339887498948482; // Knuth's multiplicative constant (from CLRS, "Introduction to Algorithms", page 232)
+    double value = key * A; // Multiply the key
+    double fractional = value - (long long)value; // Select the fractional Part
     return static_cast<int>(m * fractional);
 }
 
@@ -27,9 +27,14 @@ int myHashString(const std::string& str, int m) {
     // TODO: replace with your own design
     const double A = 0.6180339887498948482;
     for (char c : str) {
-        hash ^= (hash << 5) + (hash >> 2) + static_cast<unsigned long>(c) + 1315423911;
+        hash ^= (hash << 5) + (hash >> 2) + static_cast<unsigned long>(c) + 1315423911; //variant of PJW_hash_function 
     }
-    double v = hash * A;
-    double fractional = v - (long long)v;  // 取小數部分（不使用 floor、不使用 modf）
+    /*
+        XOR new hash content with previous hash
+        The new content each SHIFT left 5 bits and right 2 bits,
+        Final add the big constant value to mix bits. 
+    */
+    double value = hash * A;
+    double fractional = value - (long long)value;
     return static_cast<int>(m * fractional);
 }
